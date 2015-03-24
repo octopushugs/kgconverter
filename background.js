d3160c09d72cc694670ba6431303cf5d2d293056
Mike Foster <type.foster@gmail.com>
2015-03-24 12:29:30 -0700

    completed feature allowing users to convert from pounds to kg OR kg to pounds

--- background.js
+++ background.js
@@ -3,22 +3,39 @@ chrome.commands.onCommand.addListener(function(command) { //wait for user input
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {trigger: "go"}, function(response) {
 			//handles user selection and manipulates it on response
-			if(response.selection) {
-				var raw = parseFloat(response.selection);
-				if (!isNaN(raw)) { //make sure that raw is, in fact, a number before showing user anything
-					alert(raw + " kilograms is equivalent to " + raw*2.2 + " pounds.");
-				}
+			var conversionFactor = 1;
+			//fetch user preferences
+			chrome.storage.sync.get("conversion", function(fetched) {
+				var tempCon = fetched.conversion;
+				if(tempCon.includes('toKG')) {
+					conversionFactor = 0.45;
+				} 
 				else {
-					console.log("that doesn't look like a number");
+					conversionFactor = 2.2;
 				}
-			}
-			else {
-				console.log("getSelection didn't do anything");
-			}
+				if(response.selection) {
+					var raw = parseFloat(response.selection);
+					if (!isNaN(raw)) { //make sure that raw is, in fact, a number before showing user anything
+						if(conversionFactor === 2.2){
+							alert(raw + " kilograms is equivalent to " + raw*conversionFactor + " pounds.");
+						}
+						else if(conversionFactor === 0.45){
+							alert(raw + " pounds is equivalent to " + raw*conversionFactor + " kilograms.");
+						}
+						else {
+							console.log("I'm confused :(");
+						}
+					}
+					else {
+						console.log("that doesn't look like a number");
+					}
+				}
+			});
+			
+			
 		});
 	});
 });
 
-window.error = function(errorMsg, url, lineNumber) {
-	console.log("There's been an error");
-}
+
+
--- options.js
+++ options.js
@@ -1,22 +1,25 @@
 //Saves user preferences 
 function save_options() {
 	var unit = document.getElementById('units').value;
-	chrome.storate.sync.set({
+	chrome.storage.sync.set({
 		conversion: unit
 	},
 	function() {
 		//Display something so user knows preferences got saved
-		var status = getElementById('status');
+		var status = document.getElementById('status');
 		status.textContent = "Options saved!";
-		
+		setTimeout(function() {
+			status.textContent = ' ';
+		}, 750);
 	});
 }
+//When user goes to options page it is populated by their preferences
 function restore_options() {
 	chrome.storage.sync.get({
 		conversion: 'toLB'
 	},
 	function(items) {
-		document.getElementById('units').value = item.conversion;
+			document.getElementById('units').value = items.conversion;
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
